e665bc31d42fe0518128c5a3ecf1a226edf77787
vladislav-slobodenjuk <vladjun@gmail.com>
2023-12-01 17:13:38 +0200

    fix openDoor condition to use getter

--- src/4.ts
+++ src/4.ts
@@ -39,7 +39,7 @@ abstract class House {
 
 class MyHouse extends House {
   openDoor(key: IKey): void {
-    if (this.key === key) {
+    if (this.key.getSignature() === key.getSignature()) {
       this.doorIsOpen = true;
     }
   }
